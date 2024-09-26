f1d957296ed86b54aeffd0832b5512252ce5e050
John Emmanuel Marcelino <john.marcelino@ibm.com>
2024-09-26 10:30:41 +0800

    Gallery correctly renders background-image (2024-09-26 10-30-AM)

--- HomeProperty/Models/Pages/StandardPage.cs
+++ HomeProperty/Models/Pages/StandardPage.cs
@@ -1,10 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using EPiServer.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace HomeProperty.Models.Pages
 {
-    [ContentType(DisplayName = "Standard Page", GUID = "a65063f9-d042-428e-8afe-b235a6d92ff4", Description = "")]
+    [ContentType(DisplayName = "Standard Page", 
+        GUID = "a65063f9-d042-428e-8afe-b235a6d92ff4",
+        Description = "")]
     public class StandardPage : SitePageData
     {
-       
+        [CultureSpecific]
+        [Display(
+            Name = "Section Image Url",
+            Description = "Select section image",
+            GroupName = SystemTabNames.Content,
+            Order = 10)]
+        [UIHint(UIHint.Image)]
+
+        public virtual Url? MainSectionImageUrl { get; set; }
     }
 }
