c16f909f1e618277eed16c581cd6edcdcf80be72
John Emmanuel Marcelino <john.marcelino@ibm.com>
2024-09-30 12:30:00 +0800

    Addition of Property Slider (2024-09-30 12-30-PM)

---
+++ HomeProperty/Models/Blocks/PropertiesCarouselBlock.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeProperty.Models.Blocks
+{
+    [ContentType(DisplayName = "Properties Carousel Block", 
+        GUID = "9ff7bbba-8082-4b0b-bffa-636f3151fb63", 
+        Description = "")]
+    public class PropertiesCarouselBlock : BlockData
+    {
+
+        [CultureSpecific]
+        [Display(
+            Name = "Prperties Carousel Block",
+            Description = "Provide properties that will be displayed",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        [AllowedTypes(typeof(PropertiesCarouselBlockItem))]
+        public virtual ContentArea? PropertyItems { get; set; }
+
+    }
+}
