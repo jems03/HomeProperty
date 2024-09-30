0b9f7640514258fd8e22d632427eba4bfd2a1af6
John Emmanuel Marcelino <john.marcelino@ibm.com>
2024-09-30 17:03:28 +0800

    Addition of PropertyStatusHelper (2024-09-30 05-03-PM)

---
+++ HomeProperty/Utilities/PropertyStatusHelper.cs
@@ -0,0 +1,16 @@
+﻿using HomeProperty.Models.Blocks;
+
+namespace HomeProperty.Utilities
+{
+    public static class PropertyStatusHelper
+    {
+        public static string GetPropertyStatusText(string value)
+        {
+            var selectionFactory = new PropertyStatusSelectionFactory();
+            var selections = selectionFactory.GetSelections(null);
+
+            var selectedItem = selections.FirstOrDefault(s => s.Value.ToString() == value);
+            return selectedItem?.Text ?? " ";
+        }
+    }
+}
