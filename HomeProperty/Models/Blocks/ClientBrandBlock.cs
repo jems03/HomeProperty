13509467fdcc0bfbcfbf936d8a9f23ff7622428b
John Emmanuel Marcelino <john.marcelino@ibm.com>
2024-10-01 14:40:01 +0800

    Added the Client Brands (2024-10-01 02-40-PM)

---
+++ HomeProperty/Models/Blocks/ClientBrandBlock.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeProperty.Models.Blocks
+{
+    [ContentType(DisplayName = "Client Brand Block", 
+        GUID = "f824e570-daae-43ef-9522-55a893755648", 
+        Description = "")]
+    public class ClientBrandBlock : BlockData
+    {
+        [CultureSpecific]
+        [Display(
+            Name = "Client Brand Block",
+            Description = "Provide client brand logos",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual IList<ClientBrandBlockItem>? ClientBrandsList { get; set; }
+    }
+}
