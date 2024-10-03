478261026c89c3a7ced762afd045cd3ba14fa17b
John Emmanuel Marcelino <john.marcelino@ibm.com>
2024-10-03 16:13:57 +0800

    Restrict Move To Trash functioning (2024-10-03 04-14-PM)

---
+++ HomeProperty/Business/Initialization/RestrictMoveToTrash.cs
@@ -0,0 +1,37 @@
+﻿using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.Security;
+using EPiServer.ServiceLocation;
+
+namespace HomeProperty.Business.Initialization
+{
+    [InitializableModule]
+    public class RestrictMoveToTrash : IInitializableModule
+    {          
+        public void Initialize(InitializationEngine context)
+        {
+            var contentEvents = ServiceLocator.Current.GetInstance<IContentEvents>();
+            contentEvents.MovingContent += OnMovingContent;
+        }
+
+        private void OnMovingContent(object sender, ContentEventArgs e)
+        {        
+
+            // Check if the content is being moved to the Trash
+            if (e.TargetLink.CompareToIgnoreWorkID(ContentReference.WasteBasket))
+            {
+                if (!PrincipalInfo.CurrentPrincipal.IsInRole(Globals.WebRoles.WebAdmins))
+                {
+                    e.CancelAction = true;
+                    e.CancelReason = "You do not have permission to move this content to trash.";
+                }
+            }
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+            var contentEvents = ServiceLocator.Current.GetInstance<IContentEvents>();
+            contentEvents.MovingContent -= OnMovingContent;
+        }
+    }
+}
