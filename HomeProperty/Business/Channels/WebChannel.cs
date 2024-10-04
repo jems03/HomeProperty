54cd788fa9e3ef28d52ae9c185c240e7417a18ef
John Emmanuel Marcelino <john.marcelino@ibm.com>
2024-10-04 16:50:07 +0800

    Addition of Mobile and Web Channel (2024-10-04 04-50-PM)

---
+++ HomeProperty/Business/Channels/WebChannel.cs
@@ -0,0 +1,17 @@
+﻿using EPiServer.Web;
+using Wangkanai.Detection.Models;
+using Wangkanai.Detection.Services;
+
+namespace HomeProperty.Business.Channels
+{
+    public class WebChannel : DisplayChannel
+    {
+        public override string ChannelName => "web";
+
+        public override bool IsActive(HttpContext context)
+        {
+            var detection = context.RequestServices.GetRequiredService<IDetectionService>();
+            return detection.Device.Type == Device.Desktop;
+        }
+    }
+}
