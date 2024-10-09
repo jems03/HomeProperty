fa388bcd0baa1b2ca066c84ca77a9e27fe1a18ea
John Emmanuel Marcelino <john.marcelino@ibm.com>
2024-10-09 10:10:16 +0800

    Addition of the Episerver.Forms and can fetch the form submissions (2024-10-09 10-10-AM)

---
+++ HomeProperty/Components/ContactFormBlockViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using EPiServer.Web.Mvc;
+using HomeProperty.Models.Blocks;
+using HomeProperty.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeProperty.Components
+{
+    public class ContactFormBlockViewComponent : BlockComponent<ContactFormBlock>
+    {
+        protected override IViewComponentResult InvokeComponent(ContactFormBlock currentContent)
+        {
+            var model = new ContactFormViewModel(currentContent);
+
+            return View(model);
+        }
+    }
+}
