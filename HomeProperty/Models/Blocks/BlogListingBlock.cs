1788257b9ef78f9b4ee24409c07a2fedaeb77e3d
John Emmanuel Marcelino <john.marcelino@ibm.com>
2024-10-15 17:43:14 +0800

    Can now fetch the comment count with the BlogSinglePage Details (2024-10-15 05-43-PM)

--- HomeProperty/Models/Blocks/BlogListingBlock.cs
+++ HomeProperty/Models/Blocks/BlogListingBlock.cs
@@ -1,20 +1,22 @@
-﻿using EPiServer.SpecializedProperties;
+﻿using EPiServer.Data;
+using EPiServer.SpecializedProperties;
+using HomeProperty.Business.DataStore.BlogSinglePage;
 using HomeProperty.Models.Pages;
 using System.ComponentModel.DataAnnotations;
 
 namespace HomeProperty.Models.Blocks
 {
-    [ContentType(DisplayName = "Blog Listing Block", 
-        GUID = "59b30811-e23c-4d2a-b0b3-379257b43647", 
+    [ContentType(DisplayName = "Blog Listing Block",
+        GUID = "59b30811-e23c-4d2a-b0b3-379257b43647",
         Description = "")]
     public class BlogListingBlock : BlockData
-    {
-        
+    {       
         [Display(
             Name = "Blog Pages",
             GroupName = SystemTabNames.Content,
             Order = 1)]
         [AllowedTypes(typeof(BlogSinglePage))]
         public virtual IList<ContentReference> BlogSinglePages { get; set; }
+
     }
 }
