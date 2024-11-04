99f7236a604e14645f9b42183cbb39265fec3407
John Emmanuel Marcelino <john.marcelino@ibm.com>
2024-11-04 16:47:10 +0800

    Addition of Gallery Block (2024-11-04 04-47-PM)

---
+++ HomeProperty/Models/Blocks/GalleryBlock.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeProperty.Models.Blocks
+{
+    [ContentType(DisplayName = "Gallery Block", 
+        GUID = "face3e61-71ae-4483-8320-c8b771ed4957")]
+    public class GalleryBlock : BlockData
+    {
+        [CultureSpecific]
+        [Display(
+             Name = "Section Title",
+             GroupName = SystemTabNames.Content,
+             Order = 1)]
+        public virtual string? SectionTitle { get; set; }
+
+        [CultureSpecific]
+        [Display(
+            Name = "Section Descripton",
+            GroupName = SystemTabNames.Content,
+            Order = 2)]
+        [UIHint(Globals.UIHintDescriptors.TinyMCE)]
+        public virtual XhtmlString? SectionDescription { get; set; }
+    }
+}
